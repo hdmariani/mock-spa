18e652471b298b7ad753e44360d1998b7759ab4d
hdmariani <hdmariani@gmail.com>
2025-09-01 07:59:08 -0300

    Added page_view DL event

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import { useEffect } from "react";
 
 export default function App() {
+const location = useLocation();
+
+useEffect(()=>{
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: 'page_view',
+    location: location
+  }),[location]
+})
+
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900 font-sans">
       <Navbar />
