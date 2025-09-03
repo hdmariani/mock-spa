71ec154db31d491c0ab3eb3392f71cfb1af06ccb
hdmariani <hdmariani@gmail.com>
2025-09-03 18:47:55 -0300

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/mock-spa',
-=======
->>>>>>> d9d748e (initial layout)
 })
