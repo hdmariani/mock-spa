8635c8ad6b9b04739ee220cff95e459566e7ea57
hdmariani <hdmariani@gmail.com>
2025-09-03 19:04:18 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 import { Link } from "react-router";
-=======
-import { Link } from "react-router-dom";
->>>>>>> d9d748e (initial layout)
 
 export default function Navbar() {
   return (
     <nav className="bg-indigo-900 text-white px-6 py-4 flex justify-between items-center">
       <h1 className="font-bold text-xl">MockSite</h1>
       <div className="space-x-6">
-<<<<<<< HEAD
         <Link data-gtm-component= "nav_link" to="/" className="hover:text-amber-400">Home</Link>
         <Link data-gtm-component= "nav_link" to="/about" className="hover:text-amber-400">About</Link>
         <Link data-gtm-component= "nav_link" to="/contact" className="hover:text-amber-400">Contact</Link>
-=======
-        <Link to="/" className="hover:text-amber-400">Home</Link>
-        <Link to="/about" className="hover:text-amber-400">About</Link>
-        <Link to="/contact" className="hover:text-amber-400">Contact</Link>
->>>>>>> d9d748e (initial layout)
       </div>
     </nav>
   );
