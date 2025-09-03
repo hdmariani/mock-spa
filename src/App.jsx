d8766848c20f08f66d710cba3de5e351334a24f2
hdmariani <hdmariani@gmail.com>
2025-09-03 19:02:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route, useLocation, BrowserRouter } from "react-router";
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
